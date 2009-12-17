6c205a3eba7692f2303abe0ffd0bec6ce93e65ae
razor <razor@localhost.(none)>
2009-12-17 22:00:34 +0500

    *** empty log message ***

---
+++ elisp-mode.el
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; my emacs lisp mode hook ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<tab>")
+                           (lambda () (interactive)
+                             (indent-for-tab-command)
+                             (lisp-complete-symbol)))))
