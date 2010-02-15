a0cf2cf891170e708888c2e295c55cd42a078d25
segfault <razor@localhost.(none)>
2010-02-16 00:14:11 +0500

    modified:   my-c-mode.el 	modified:   my-org-mode.el

--- my-c-mode.el
+++ my-c-mode.el
@@ -1,14 +1,15 @@
 ;;;;;;;;;;;;;;;;;;;;
 ;; my c mode hook ;;
 ;;;;;;;;;;;;;;;;;;;;
-(add-hook 'c-mode-hook
-          (lambda ()
-            (semantic-load-enable-code-helpers)
-            (local-set-key (kbd "<tab>")
-                           (lambda () (interactive)
-                             (indent-for-tab-command)
-                             (semantic-complete-analyze-inline)))
-            (local-set-key (kbd "<C-return>") 'semantic-ia-fast-jump)
-            (local-set-key (kbd "<C-f1>") 'pop-mark)
-            (local-set-key (kbd "<C-f2>") 'pop-global-mark)
-            (local-set-key (kbd "<C-tab>") 'yas/expand)))
+(defun my-c-mode-hook ()
+  (semantic-load-enable-code-helpers)
+  (local-set-key (kbd "<tab>")
+                 (lambda () (interactive)
+                   (indent-for-tab-command)
+                   (semantic-complete-analyze-inline)))
+  (local-set-key (kbd "<C-return>") 'semantic-ia-fast-jump)
+  (local-set-key (kbd "<C-f1>") 'pop-mark)
+  (local-set-key (kbd "<C-f2>") 'pop-global-mark)
+  (local-set-key (kbd "<C-tab>") 'yas/expand))
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c++-mode-hook 'my-c-mode-hook)
--- my-org-mode.el
+++ my-org-mode.el
@@ -1,4 +1,7 @@
 (add-hook 'org-mode-hook (lambda ()
                            (local-set-key (kbd "C-o") (lambda () (interactive)
                                                         (move-end-of-line 1)
-                                                        (org-return)))))
+                                                        (org-return)))
+                           (local-set-key (kbd "<C-S-return>") (lambda () (interactive)
+                                                                 (move-end-of-line 1)
+                                                                 (org-insert-todo-heading-respect-content)))))
