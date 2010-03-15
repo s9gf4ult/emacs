5cccc583c21b7be0ee0b497b3223694f19851419
segfault <razor@localhost.(none)>
2010-03-16 01:00:15 +0500

    modified:   ../my-python-mode.el 	renamed:    ../pycomplete.el -> pycomplete.el 	new file:   pycomplete.py

--- my-python-mode.el
+++ my-python-mode.el
@@ -1,6 +1,4 @@
 (add-hook 'python-mode-hook (lambda ()
-                              (load "ipython")
-                              (setq ipython-completion-command-string "print(';'.join(__IP.Completer.all_completions('%s')))\n")
                               (local-set-key (kbd "<return>")
                                              (lambda () (interactive)
                                                (newline-and-indent)))))
