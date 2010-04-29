3088114164d4fbe29868455ad303b8ff1c190ec0
segfault <razor@localhost.(none)>
2010-04-29 23:06:06 +0600

    modified:   init.el 	new file:   my-slime-mode.el

--- init.el
+++ init.el
@@ -106,6 +106,7 @@
 (load "my-org-mode")
 (load "my-python-mode")
 (load "my-sh-mode")
+(load "my-slime-mode")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; customize settings ;;
@@ -129,12 +130,6 @@
  '(use-file-dialog nil)
  '(woman-fill-column (- (window-width) 5))
  '(woman-use-own-frame nil))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
 
 
 
---
+++ my-slime-mode.el
@@ -0,0 +1,3 @@
+(add-hook 'slime-mode-hook (lambda () (interactive)
+                             "when slime starts this function is runs"
+                             (local-set-key (kbd "<tab>") 'slime-complete-symbol)))
