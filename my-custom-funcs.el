fac858b8b7387b6d01de20cb34b757890e9d4e53
segfault <razor@localhost.(none)>
2010-05-06 12:05:18 +0600

    kill-all-buffers-by-regexp

--- my-custom-funcs.el
+++ my-custom-funcs.el
@@ -41,7 +41,18 @@
       (let ((pull-point (point)))
         (yank)
         (goto-char (+ pull-point current-fun-diff))))))
-
+(require 'cl)
+(defun kill-all-buffers-by-regexp (regexp &optional buffers-to-kill)
+  "Kills all buffers whitch names matches by `regexp'.
+If `buffers-to-kill' is not nil then kills just buffers from this list.
+If `buffers-to-kill' is nil or t then kills buffers from `(buffer-list)'"
+  (interactive "MRegexp: ")
+  (let ((process-buffers (if (and buffers-to-kill
+                                  (not (eql t buffers-to-kill)))
+                            buffers-to-kill
+                          (buffer-list))))
+    (dolist (cbuffer (loop for var in process-buffers when (string-match regexp (buffer-name var)) collect var))
+      (kill-buffer cbuffer))))
 
 
   
