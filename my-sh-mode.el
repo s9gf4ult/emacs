9b3b1e9a4acd2b9b844b3d19f23828beeda0e224
segfault <razor@calculate.local>
2010-06-11 16:33:26 +0600

    C-tab for just tab

--- my-sh-mode.el
+++ my-sh-mode.el
@@ -1,4 +1,8 @@
 (add-hook 'sh-mode-hook (lambda ()
-                          (local-set-key (kbd "M-p") 'comint-dynamic-complete-filename)))
+                          (local-set-key (kbd "M-p") 'comint-dynamic-complete-filename)
+                          (local-set-ket (kbd "<C-tab>") (lambda () (interactive)
+                                                           (insert "\t")))))
+
+                         
 
 (provide 'my-sh-mode)
