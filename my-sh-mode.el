3b288658e578d784927cba139081f81339660928
segfault <razor@localhost.(none)>
2010-08-27 23:56:13 +0600

    some fix

--- my-sh-mode.el
+++ my-sh-mode.el
@@ -4,7 +4,7 @@
 
 (add-hook 'sh-mode-hook (lambda ()
                           (local-set-key (kbd "M-p") 'comint-dynamic-complete-filename)
-                          (local-set-ket (kbd "<C-tab>") (lambda () (interactive)
+                          (local-set-key (kbd "<C-tab>") (lambda () (interactive)
                                                            (insert "\t")))))
 
 (provide 'my-sh-mode)
