4eb9e0192e432c39c75c95a9652cc4cf5acdcd78
segfault <s9gf4ult@gmail.com>
2012-01-06 01:53:07 +0600

    kill-all-buffers-by-regexp

--- my-custom-funcs.el
+++ my-custom-funcs.el
@@ -2,17 +2,6 @@
 ;; my-custom-funcs ;;
 ;;;;;;;;;;;;;;;;;;;;;
 
-(defun kill-all-buffers-by-regexp (regexp &optional buffers-to-kill)
-  "Kills all buffers whitch names matches by `regexp'.
-If `buffers-to-kill' is not nil then kills just buffers from this list.
-If `buffers-to-kill' is nil or t then kills buffers from `(buffer-list)'"
-  (interactive "MRegexp: ")
-  (let ((process-buffers (if (and buffers-to-kill
-                                  (not (eql t buffers-to-kill)))
-                            buffers-to-kill
-                          (buffer-list))))
-    (dolist (cbuffer (loop for var in process-buffers when (string-match regexp (buffer-name var)) collect var))
-      (kill-buffer cbuffer))))
 
 (defun mark-line (arg)
   "marks current line (at point) or more that one of `arg' is set to >0"
--- s9g-defuns.el
+++ s9g-defuns.el
@@ -51,8 +51,19 @@
   (add-to-list 'load-path (concat dotfiles-dir
 				  subdir)))
 
-(provide 's9g-defuns)
-;;; s9g-defuns.el ends here
-=======
+(defun kill-all-buffers-by-regexp (regexp &optional buffers-to-kill)
+  "Kills all buffers whitch names matches by `regexp'.
+If `buffers-to-kill' is not nil then kills just buffers from this list.
+If `buffers-to-kill' is nil or t then kills buffers from `(buffer-list)'"
+  (interactive "MRegexp: ")
+  (let ((process-buffers (if (and buffers-to-kill
+                                  (not (eql t buffers-to-kill)))
+                            buffers-to-kill
+                          (buffer-list))))
+    (loop for var in process-buffers
+          when (string-match regexp (buffer-name var))
+          do (kill-buffer var))))
+
+
 (provide 's9g-defuns)
 ;;; s9g-defuns.el ends here
