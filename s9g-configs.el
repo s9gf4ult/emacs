b37e6e844258ef4933905050f64915758bdf05fb
segfault <s9gf4ult@gmail.com>
2012-01-19 16:31:46 +0600

    hl-line mode

--- s9g-configs.el
+++ s9g-configs.el
@@ -39,6 +39,7 @@
  '(desktop-save-mode t)
  '(ecb-options-version "2.40")
  '(emacs-lisp-mode-hook (quote (turn-on-eldoc-mode (lambda nil (local-set-key (kbd "TAB") (function (lambda nil (interactive) (if mark-active (indent-region (region-beginning) (region-end)) (if (looking-at "\\_>") (completion-at-point) (indent-for-tab-command))))))))))
+ '(global-hl-line-mode t)
  '(global-semantic-decoration-mode t nil (semantic-decorate-mode))
  '(global-semantic-highlight-edits-mode nil nil (semantic-util-modes))
  '(global-semantic-highlight-func-mode t nil (semantic-util-modes))
