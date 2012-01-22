67fb73dc8b067fde73d5302c7af1bca31d5fbd09
segfault <s9gf4ult@gmail.com>
2012-01-22 22:34:25 +0600

    cursor is white

--- s9g-configs.el
+++ s9g-configs.el
@@ -104,7 +104,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(cursor ((t (:inverse-video t))))
+ '(cursor ((t (:background "white"))))
  '(diff-added ((t (:background "#38633A"))))
  '(diff-changed ((t (:foreground "#594004"))))
  '(diff-removed ((t (:background "#3D0002"))))
