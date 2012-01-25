19ca453ab95dac921d8be0f33b01f93eac3d734e
segfault <s9gf4ult@gmail.com>
2012-01-25 10:10:07 +0600

    modified:   s9g-configs.el 	white cursor

--- s9g-configs.el
+++ s9g-configs.el
@@ -104,6 +104,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "white"))))
  '(diff-added ((t (:background "#162B16"))))
  '(diff-changed ((nil (:background "#2B2A16"))))
  '(diff-removed ((t (:background "#2B1617"))))
