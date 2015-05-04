eec57b123d3d92c74241a607ecd23cab61c4435a
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-05-04 19:01:34 +0500

    ac-haskell-process

--- s9g-auto-complete.el
+++ s9g-auto-complete.el
@@ -3,9 +3,9 @@
 
 (setq-default
  ac-sources
- '(ac-source-dictionary
+ '(ac-source-haskell-process
+   ac-source-dictionary
    ac-source-words-in-same-mode-buffers
-   ac-source-haskell-process
    ))
 
 (provide 's9g-auto-complete)
