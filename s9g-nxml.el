477a0b8462de41d3fccd71e9966e48dc0965bdba
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-06-15 16:01:49 +0500

    folding in xml

---
+++ s9g-nxml.el
@@ -0,0 +1,51 @@
+;;; s9g-nxml.el ---                                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015
+
+;; Author:  <razor@gazoline>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+
+
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "<tab>") 'hs-toggle-hiding)
+(define-key nxml-mode-map (kbd "C-c s") 'hs-show-all)
+
+
+(provide 's9g-nxml)
+;;; s9g-nxml.el ends here
